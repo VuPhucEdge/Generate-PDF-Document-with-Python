c1fe70c022594abecf56403afb272a1b3c35aa36
VuPhuc <vuphucjava@gmail.com>
2023-06-23 22:22:56 +0700

    add line

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 20, 200, 22)
+
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 
     # Set footer
     pdf.ln(277)
@@ -30,4 +32,7 @@ for index, row in df.iterrows():
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=0, txt=row["Topic"], align="R")
 
-pdf.output('output.pdf')
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
+
+pdf.output("output.pdf")
